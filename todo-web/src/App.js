e39717890354fc0ff48c9f1fc10b8c0a15606530
skyriv213 <gn1007@dankook.ac.kr>
2023-04-06 18:19:59 +0900

    <feat> React - Mock Todo 활용해보기, 컴포넌트를 클래스 타입이 아닌 arrow function으로 작성해보기

--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'; 
+import Todo from './Todo';
 import './App.css';
 
-function App() {
+const App = ()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>왜 안됨?</h1>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <Todo />
+//      <Todo />
+//      <Todo />
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ todo-web/src/Todo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Todo = () => {
+    return (
+    <div className="Todo">
+        <input type="checkbox" id="todo0" name="todo0" value="todo0" />
+        <label for ="todo0">make Todo component</label>
+    </div>
+    )
+}
+export default Todo;
+
